56d85405693bba83b32bdc2ca393642ff792ce05
Guilherme Dravanete Silva - 824136863 <824136863@ulife.com.br>
2025-03-11 10:12:38 -0300

    Feath: Trabalhando com variaveis

---
+++ variaveis.js
@@ -0,0 +1,15 @@
+var idade = 19;
+let nome = "Guilherme";
+let ativo = true;
+const PI = 3.14;
+
+
+
+if(true){
+    let cidade = "osasco";
+    console.log(idade);
+    console.log(nome);
+    console.log(ativo);
+    PI = 2;
+    console.log(PI);
+}
