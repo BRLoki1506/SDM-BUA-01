17498b6948cd270ba6c787cccc4f3cea4cc41ef2
Guilherme Dravanete Silva - 824136863 <824136863@ulife.com.br>
2025-03-18 10:32:51 -0300

    trabalhando com Javascript

---
+++ colecao.js
@@ -0,0 +1,16 @@
+let map = new Map()
+
+map.set("nome", "Danilo")
+map.set("idade", 39)
+
+console.log(map.get("nome"))
+console.log(map.has("idade"))
+console.log(map.size)
+
+map.forEach((valor, chave)=>{
+    console.log(`${chave}: ${valor}`)
+})
+
+//remover elemantos do map
+map.clear()
+console.log(map.size)
---
+++ funcoes.js
@@ -0,0 +1,16 @@
+function ola(nome){
+    return `Olá, ${nome}`
+}
+
+console.log(ola("Ronaldinho"))
+
+const soma = function(a, b){
+    return a + b
+}
+
+console.log(soma(50, 32))
+
+
+const mult = (x, y) => x * y;
+
+console.log(mult(10, 25))
